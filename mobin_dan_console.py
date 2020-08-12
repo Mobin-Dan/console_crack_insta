a2d6095a0617ae5826a01e7968094b4d8d29f9aa
Mobin-Dan <68151748+Mobin-Dan@users.noreply.github.com>
2020-08-12 20:27:20 +0430

    Mobin_dan_console

---
+++ mobin_dan_console.py
@@ -0,0 +1,29 @@
+import os
+z='\033[92m'
+a='\033[0m'
+os.system("toilet mobin_dan_console"+z)
+os.system("figlet YouTuBe"+a)
+print"https://www.youtube.com/channel/UCrts8wi8GCzrMXDn9BldMTw"+a
+
+print"show  for show"
+f=raw_input("mobin_dan==>"+z)
+if f=="show":
+	
+	print"crack insta"+a
+	print"phishing insta"+z
+	print"proxy_combo_passlist"+a
+x=raw_input("mobin_dan==>"+z)
+if x=="crack insta":
+	os.system("https://github.com/Pure-L0G1C/Instagram")
+	os.system("cd Instagram")
+	os.system("pip install -r requirements.txt")
+	os.system("python instagram.py -h")
+if x=="phishing insta":
+	os.system("git clone https://github.com/thelinuxchoice/shellphish")
+	os.system("cd shellphish")
+	os.system("pkg install php")
+	os.system("bash shellphish.sh")
+if x=="proxy_combo_passlist":
+	os.system("git clone https://github.com/Mobin-Dan/pass_combo_proxy")
+	os.system("cd pass_combo_proxy")
+	os.system("ls")
